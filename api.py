6d66b84a73a3670d387f62432ff56dcc1d429a1b
Alex <medinas.alex0001@gmail.com>
2019-05-20 18:42:43 -0500

    creacion de api

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import json
+
 
 tt = Flask(__name__)
 api = Api(tt)
@@ -7,12 +9,17 @@ api = Api(tt)
 from flask import request
 @tt.route("/datos",methods=['POST'])
 def datos():
-    values = request.get_json()
+    print (request.is_json)
+    values = request.form.to_dict()
+    print(request.headers)
+    print(values)
+
+    #print(json.loads(values))
     respuesta = {'clase':'A','idLsm':1, 'idResult':3}
     return json.dumps(respuesta)
 
 if __name__ == '__main__':
- tt.run(debug=True)
+ tt.run('0.0.0.0', 5000, debug=True)
 
 
 
